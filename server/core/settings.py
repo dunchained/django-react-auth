aa5fa837917e93bab54149ae3d9f18a34d6fe2af
Mirza Uzunović <uzunovic.mirza@gmail.com>
2024-07-24 20:19:18 +0200

    iniciro si

--- server/core/settings.py
+++ server/core/settings.py
@@ -22,7 +22,7 @@ from decouple import config
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '29(*$ib*7r*uewbylmti*q1i$l9!o^6x--hph11fb7u(7@6*co'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
